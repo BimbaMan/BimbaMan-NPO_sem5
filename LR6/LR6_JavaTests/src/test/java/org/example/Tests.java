6908b52bf9195ffd6ba307254053baf037495696
BimbaMan <ivan.d.sidorenko@gmail.com>
2022-12-12 12:43:06 +0300

    LR6 java

--- LR6/LR6_JavaTests/src/test/java/org/example/Tests.java
+++ LR6/LR6_JavaTests/src/test/java/org/example/Tests.java
@@ -8,29 +8,31 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
-// page_url = http://lyceum2.minsk.edu.by/
+// http://lyceum2.minsk.edu.by/
 public class Tests {
 
-    private static final String SCHOOL_URL ="http://lyceum2.minsk.edu.by/";
+        private static final String SCHOOL_URL = "http://lyceum2.minsk.edu.by/";
 
-    public Tests(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get(SCHOOL_URL);
-        webDriver.manage().window().maximize();
-        Thread.sleep(500);
-
-        webDriver.findElement(By.className("stranica_3")).click();
-        Thread.sleep(500);
+        public Tests(WebDriver driver) {
+            PageFactory.initElements(driver, this);
+        }
 
 
+        public static void main(String[] args) throws Exception {
+            WebDriver webDriver = new FirefoxDriver();
+            webDriver.get(SCHOOL_URL);
+            webDriver.manage().window().maximize();
+            Thread.sleep(500);
+            webDriver.findElement(By.id("versionForVisuallyImpaired")).click();
+            Thread.sleep(1000);
 
+            for (int i = 0; i < 10; i++) {
+                webDriver.findElement(By.className("bx-next")).click();
+                Thread.sleep(1500);
+            }
+            Thread.sleep(2000);
 
+            webDriver.close();
+        }
 
-        webDriver.close();
-    }
 }
